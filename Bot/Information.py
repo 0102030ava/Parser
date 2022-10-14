75de8278a96441511485acde1df6a7d73f947fac
Nuu8rs <115722824+Nuu8rs@users.noreply.github.com>
2022-10-15 01:16:30 +0300

    Update Information.py

--- Bot/Information.py
+++ Bot/Information.py
@@ -1,6 +1,6 @@
 
-token = "5746943692:AAEERs0B_PuYnPluACKxUs9jzkJ9hqvGI0Q"
+token = "Token"
 active_use_pars = []
 arr_users = {}
 arr_stop = {}
-user_admin = [5456085368,674645538,1144389227,878606546,5489731156]
+user_admin = ['']
